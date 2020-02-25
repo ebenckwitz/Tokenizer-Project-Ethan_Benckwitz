c477b53f5e2ac757ad009085f2d7ba1974119d8b
Ethan <ebenckwitz>
2020-02-25 15:25:57 -0700

    updated with third milestone

--- src/milestone2.c
+++ src/milestone2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifndef _TOKENIZER_
 #define _TOKENIZER_
@@ -7,11 +8,9 @@
 
 /*This method will get the length of the entire string*/
 int word_length(char *str) {
-  int total = 0;
-  while(str[total] != '\0') {
-    total++;
-  }
-  return total;
+  int i;
+  for (i = 0; str[i] != '\0'; ++i);
+  return i;
 }
 
 /*This method will return true (non-zero) if c is
@@ -25,30 +24,27 @@ int space_char(char c) {
 whitespace character (not tab or space). Zero terminators
 are not printable (therefore false)*/
 int non_space_char(char c) {
-  return (c != 0x20 || c != "\t") ? true : false;
+  return (space_char(c) != true) ? true : false;
 }
 
 /*Returns a pointer to the first character of 
 the word inside the string. Returns a zero 
 pointer if str does not contain any words. */
 char *word_start(char *str) {
-  int j = 0;
-  char *ip;
-  ip = &str[j];
-  return *ip;
+  for (; space_char(*str) || *str == '\0'; str++) {
+    ;
+  }
+  return str;
 }
 
 /*Returns a pointer to the last character in the
 last word in the entire string. Returns a zero pointer
 if str does not contain any space characters. */
 char *word_end(char *str) {
-  int i = 0;
-  for(i; i < word_length(str) -  1; i++) {
+  for (; non_space_char(*str) || *str == '\0'; str++) {
     ;
   }
-  char *ip;
-  ip = &str[i];
-  return *ip;
+  return str;
 }
 
 /*This method counts the number
@@ -61,6 +57,49 @@ int count_words(char *str) {
     }
   }
   return total;
+}
+/*turns the str that is passed into a copy pointer to be
+used in the tokenize method */
+char *copy_str(char *inStr, short len) {
+  char *copy;
+  copy = (char*) malloc (len + 1);
+  int i;
+  for (i = 0; i < len; inStr++, i++) {
+    copy[i] = *inStr;
+  }
+  copy[i] = '\0';
+  return copy;
+}
+
+/*Passes in a str and this method will break down each word an put it 
+token array that has each word seperated */
+char **tokenize(char* str) {
+  int total = count_words(str);
+  char **token;
+  token = (char**) malloc (total);
+  int i;
+  for(i = 0; i < total; i++) {
+    char* s = word_start(str);
+    char *e = word_end(s);
+    token[i] = copy_str(s, (e - s));
+    str = word_start(e);
+  }
+  return token;
+}
+
+/*prints all tokens. */
+void print_tokens(char **tokens) {
+  for (int i = 0; *(tokens + i) != '\0'; i++) {
+    printf("tokenize word #%d: %s\n", (i + 1), tokens[i]);
+  }
+}
+
+/*releases all tokens using free */
+void free_tokens(char **tokens) {
+  for (int i = 0; *(tokens + i) != '\0'; i++) {
+    free(*(tokens + i));
+  }
+  free(tokens);
 }
 
 #endif
